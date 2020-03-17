05e926f3f505793c7fab804f19b9739de04ab7e9
x <2433797W@student.gla.ac.uk>
2020-03-17 18:26:36 +0000

    barrating

--- static/page.js
+++ static/page.js
@@ -1,5 +1,14 @@
  $(function() {
-      $('#example').barrating({
+      $('#rating').barrating({
           theme: 'fontawesome-stars',
       });
    });
+$('#rating').barrating('show', {
+    theme: 'fontawesome-stars',
+    onSelect: function(value, text, event) {
+        if (typeof(event) !== 'undefined') {
+            // rating was selected by a user
+            console.log(value);
+        }
+    }
+});
